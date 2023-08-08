245800c3e037c549ae4087c8f65ff89b366dd7cb
LucasJG1994 <lgaraway@yahoo.com>
2023-08-08 07:10:56 +0100

    Updated Lua Implementation

--- src/main.c
+++ src/main.c
@@ -4,25 +4,23 @@
 
 
 int main(int argc, char** argv) {
-	if (argc == 2) {
-		lua_State* L = luaL_newstate(); //Creates a new Lua State
-		luaL_openlibs(L);               //Opens the Lua Standard Libraries
+	lua_State* L = luaL_newstate(); //Creates a new Lua State
+	luaL_openlibs(L);               //Opens the Lua Standard Libraries
 
-		if (luaL_loadfile(L, argv[1]) != LUA_OK) {
-			printf("%s\n", lua_tostring(L, -1));
-			lua_close(L);
-			return 0;
-		}
-
-		lua_pcall(L, 0, 0, 0);
-		lua_getglobal(L, "update");
-
-		while (lua_pcall(L, 0, 0, 0) == LUA_OK) {
-			lua_getglobal(L, "update");
-		}
-
+	if (luaL_loadfile(L, "tests/test.lua") != LUA_OK) { //If file does not exist. Close the Lua Library and exit
+		printf("%s\n", lua_tostring(L, -1));
 		lua_close(L);
 		return 0;
 	}
 
+	/*
+		Register C function into Lua here.
+
+		lua_register(L, <function name as string>, <function name in code>);
+	*/
+
+	lua_pcall(L, 0, 0, 0); // Execute lua code loaded from file.
+
+	lua_close(L);
+	return 0;
 }
